da803ed7dd5795311fcf38b1f6d8c0bbb5ccf07d
Travis Courtney <travis@angry-fly.com>
2021-04-17 23:45:42 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+let findMatching = (names, string) => {
+     return names.filter(function(names) {return names.toLowerCase() === string.toLowerCase()})
+}
+
+let fuzzyMatch = (names, string) => {
+    return names.filter(function(names) {
+        if (names.substring(0, string.length) === string) {
+            return names
+        }
+    })
+}
+
+let matchName = (names, string) =>   {
+    return names.filter(function(names) {
+        if(names.name === string) {
+            return names.hometown
+        }
+    })
+}
